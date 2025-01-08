f603ea3714bb5beed0f8aa4475358d7a1636879a
Libra-lsp <lishipeng1012@outlook.com>
2025-01-08 11:37:14 +0800

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,27 +4,32 @@ import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default tseslint.config({
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, eslintPluginPrettierRecommended],
-    files: ["src/**/*.{ts,tsx,js}"],
-    ignores: ["dist", "node_modules"],
-    languageOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            // myCustomGlobal: "readonly"
+export default [
+    ...tseslint.config({
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, eslintPluginPrettierRecommended],
+        files: ["src/**/*.{ts,tsx,js}"],
+        ignores: ["dist", "node_modules"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                // myCustomGlobal: "readonly"
+            },
         },
-    },
-    plugins: {
-        //
-    },
-    rules: {
-        "prettier/prettier": "warn", // 默认为 error
-        "arrow-body-style": "off",
-        "prefer-arrow-callback": "off",
+        plugins: {
+            //
+        },
+        rules: {
+            "prettier/prettier": "warn", // 默认为 error
+            "arrow-body-style": "off",
+            "prefer-arrow-callback": "off",
 
-        "@typescript-eslint/no-explicit-any": "off", // allow any type
+            "@typescript-eslint/no-explicit-any": "off", // allow any type
+        },
+    }),
+    {
+        ignores: ["dist", "node_modules"],
     },
-});
+];
